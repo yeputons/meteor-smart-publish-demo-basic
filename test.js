5359af1aea7d2b028a2921b09def16faf79ba804
Egor Suvorov <egor.suvorov@gmail.com>
2015-10-20 17:48:22 +0300

    Re-design and add clarifying comment

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ Authors = new Mongo.Collection('author');
 if (Meteor.isClient) {
   Template.main.helpers({
     items: function() {
-      return Items.find({}, {sort: ['authorId', 'allowSecret', '_id']});
+      return Items.find({}, {sort: ['authorId', 'allowExtra', '_id']});
     },
     authors: function() {
       return Authors.find();
@@ -25,20 +25,22 @@ if (Meteor.isServer) {
   if (Items.find().count() == 0) {
     var ids = [];
     for (var i = 0; i < 10; i++) {
-      ids.push(Authors.insert({id: i, secret: i}));
+      ids.push(Authors.insert({id: i, extra: 'Extra for ' + i}));
     }
     for (var i = 0; i < 30; i++) {
       var a = Math.floor(Math.random() * (10 - 1e-9));
-      Items.insert({author: ids[a], authorId: a, enabled: false, allowSecret: Math.random() < 0.5});
+      Items.insert({author: ids[a], authorId: a, enabled: false, allowExtra: Math.random() < 0.5});
     }
   }
   Meteor.publish('items-disabled', function() {
     return Items.find({enabled: false});
   });
   Meteor.smartPublish('items-enabled', function() {
-    this.addDependency('items', ['author', 'allowSecret'], function(item) {
+    this.addDependency('items', ['author', 'allowExtra'], function(item) {
       fields = {};
-      if (!item.allowSecret) fields['secret'] = 0;
+      if (!item.allowExtra) {
+        fields['extra'] = 0;
+      }
       return Authors.find(item.author, {fields: fields});
     });
     return Items.find({enabled: true});
